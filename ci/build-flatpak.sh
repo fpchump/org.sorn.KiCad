a57fa21eda5110f66bc32cc07c4991880d0ccb89
Hong-She Liang <starofrainnight@gmail.com>
2018-11-09 14:44:26 +0800

    Try to build inside docker container for avoid ftp download problem
    Reference to https://blog.travis-ci.com/2018-07-23-the-tale-of-ftp-at-travis-ci

---
+++ ci/build-flatpak.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+add-apt-repository -y ppa:alexlarsson/flatpak
+apt update
+apt install -y flatpak flatpak-builder
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.gnome.Platform/x86_64/3.26
+flatpak install -y flathub org.gnome.Platform.Locale/x86_64/3.26
+flatpak install -y flathub org.gnome.Sdk/x86_64/3.26
+flatpak install -y flathub org.gnome.Sdk.Locale/x86_64/3.26
+flatpak-builder --repo=repo ./build org.sorn.KiCad.yml
+flatpak build-bundle repo kicad.flatpak org.sorn.KiCad
